c2cf900e28d0093074a91b0cbaaa1f6cbcee58be
Vroom <harrison.hilpert@gmail.com>
2024-03-03 01:13:05 -0800

    Basic left-right movement

--- src/main.py
+++ src/main.py
@@ -1,39 +1,67 @@
 from os import path
+from enum import Enum
 
 import pygame
 import pygame.gfxdraw
 
+class InputTypes(Enum):
+    KEYDOWN = 0
+    KEYUP = 1
+
+    WAD = 2 # WASD but W is jump and there is no "D"
+    LUR = 3 # Left, Up, Right
+
 class Player:
-    def __init__(self, initPos: tuple[int, int], texture: str) -> None:
+    def __init__(self, initPos: tuple[int, int], texture: str, schema: InputTypes) -> None:
         self.rect = pygame.Rect(initPos[0], initPos[1], 64, 64)
         self.surface = pygame.image.load(path.join("Sprite_Imgs", texture))
 
+        self.schema = schema
+
         self.velocityX = 0
+        self.MOVEMENTSPEED = 100
         self.ambientFriction = 50
 
         self.velocityY = 0
         self.gravityAcc = 60
         self.gravityCap = 100
 
-    def control(self, ):
-        pass
+        self.applyFriction = False
+
+    def control(self, key, mode: InputTypes, dt):
+        if mode == InputTypes.KEYUP:
+            self.applyFriction = True
+        else:
+            self.applyFriction = False
+
+        if self.schema == InputTypes.WAD:
+            match key:
+                case pygame.K_a:
+                    self.velocityX = -self.MOVEMENTSPEED * dt
+                
+                case pygame.K_d:
+                    self.velocityX = self.MOVEMENTSPEED * dt
+
+        else:
+            print("LUR Input Schema")
 
     def movementUpdate(self, deltaTime: float):
         if self.velocityY < self.gravityCap:
             self.velocityY += int(self.gravityAcc * deltaTime)
 
-        if self.velocityX > 0:
-            self.velocityX -= 50*deltaTime
+        if self.applyFriction == True:
+            if self.velocityX > 0:
+                self.velocityX -= 50*deltaTime
 
-            # If overcorrected reset
-            if self.velocityX < 0: 
-                self.velocityX = 0
-        elif self.velocityX < 0:
-            self.velocityX += 50*deltaTime
+                # If overcorrected reset
+                if self.velocityX < 0: 
+                    self.velocityX = 0
+            elif self.velocityX < 0:
+                self.velocityX += 50*deltaTime
 
-            # If overcorrected reset
-            if self.velocityX > 0: 
-                self.velocityX = 0
+                # If overcorrected reset
+                if self.velocityX > 0: 
+                    self.velocityX = 0
         
         if self.rect.y+64 >= 720: # 720 is arbirary floor for skake of testing
             self.velocityY = 0
@@ -49,7 +77,7 @@ def main():
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
 
-    playerOne = Player((100, screen.get_height()/2), "blankSprite.jpeg")
+    playerOne = Player((100, screen.get_height()/2), "blankSprite.jpeg", InputTypes.WAD)
 
     dt: float = 0
     running = True
@@ -60,7 +88,10 @@ def main():
                     running = False
                 
                 case pygame.KEYDOWN:
-                    playerOne.control()
+                    playerOne.control(event.key, InputTypes.KEYDOWN, dt)
+
+                case pygame.KEYUP:
+                    playerOne.control(event.key, InputTypes.KEYUP, dt)
                 
         screen.fill((145, 194, 158))
 

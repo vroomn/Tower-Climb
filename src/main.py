08f09ee9d3fc321d5cadcc013c88a5eb96233a97
Vroom <harrison.hilpert@gmail.com>
2024-03-03 00:25:22 -0800

    Basic Gravity and Spriting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,40 @@
+from os import path
+
 import pygame
+import pygame.gfxdraw
+
+class Player:
+    def __init__(self, initPos: tuple[int, int], texture: str) -> None:
+        self.rect = pygame.Rect(initPos[0], initPos[1], 64, 64)
+        self.surface = pygame.image.load(path.join("Sprite_Imgs", texture))
+
+        self.velocityX = 0
+        self.velocityY = 0
+        self.gravityAcc = 60
+        self.gravityCap = 100
+
+    def movementUpdate(self, deltaTime: float):
+        if self.velocityY < self.gravityCap:
+            self.velocityY += int(self.gravityAcc * deltaTime)
+        
+        if self.rect.y+64 >= 720:
+            self.velocityY = 0
+            self.rect = self.rect.move(0, (720 - (self.rect.y+64)))
+
+        self.rect = self.rect.move(self.velocityX, self.velocityY)
+
+    def draw(self, drawSurface: pygame.Surface):
+        drawSurface.blit(self.surface, self.rect)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
-    running = True
 
+    playerOne = Player((10, 0), "blankSprite.jpeg")
+
+    dt: float = 0
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,8 +42,12 @@ def main():
 
         screen.fill((145, 194, 158))
 
+        playerOne.movementUpdate(dt)
+
+        playerOne.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60)/1000
     
     pygame.quit()
 
